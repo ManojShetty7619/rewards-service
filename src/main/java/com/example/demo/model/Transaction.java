865a59f83dd0de8de20f6273bcd0a1b9e4eecc5c
Manoj Shetty <manojshetty5163@gmail.com>
2026-02-23 11:09:01 +0530

    Changes for Review comments

--- src/main/java/com/example/demo/model/Transaction.java
+++ src/main/java/com/example/demo/model/Transaction.java
@@ -20,44 +20,4 @@ public class Transaction {
 
 	private LocalDate txnDate;
 
-	public Long getTxnId() {
-		return txnId;
-	}
-
-	public void setTxnId(Long txnId) {
-		this.txnId = txnId;
-	}
-
-	public Long getCustomerId() {
-		return customerId;
-	}
-
-	public void setCustomerId(Long customerId) {
-		this.customerId = customerId;
-	}
-
-	public Long getProductId() {
-		return productId;
-	}
-
-	public void setProductId(Long productId) {
-		this.productId = productId;
-	}
-
-	public double getAmount() {
-		return amount;
-	}
-
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
-
-	public LocalDate getTxnDate() {
-		return txnDate;
-	}
-
-	public void setTxnDate(LocalDate txnDate) {
-		this.txnDate = txnDate;
-	}
-
 }
